bef80f6dbb55ab67094fca2a45887a4f19453f53
nikhilraj021 <dnraj2001@gmail.com>
2025-08-21 20:21:57 +0530

    installed ejs and added static file public

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ import {connectDB} from './config/mongodb.js'
 
 const app = express();
 const PORT = 3000;
+app.use(express.static('public'))
 
 // Connect to MongoDB, if we use await infront of connectBD,db will be connected first then server.
 connectDB()
 
 app.get("/", (req, res) => {
     res.send("Hello World")
+})
+
+app.get('/url', (req, res) => {
+    res.render('form.ejs')
 })
 
 app.listen(PORT, () => {
